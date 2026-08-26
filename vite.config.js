db810c9abd28a806e67abe1b5c2cffe8229749ae
SahilMirza1779 <sahilmirza29365@gmail.com>
2026-08-26 15:22:50 +0530

    Added PWA support to convert portfolio into an installable app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      // 🚀 YAHAN HACK ADD KIYA HAI 🚀
+      devOptions: {
+        enabled: true, // Ye line localhost par PWA testing on kar degi
+      },
+      includeAssets: ["sahil_favicon.png"],
+      manifest: {
+        name: "Sahil Mirza Portfolio",
+        short_name: "Sahil",
+        description: "MERN & .NET Developer Portfolio of Sahil Mirza",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        icons: [
+          {
+            src: "/sahil_favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/sahil_favicon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
